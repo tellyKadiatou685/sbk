3b9d03d2b33161b3f43c37378c88c506bc2bb941
Safiatoutelly <safiatoutelly685@gmail.com>
2025-09-28 12:26:16 +0000

    dddddd

--- api/cron.js
+++ api/cron.js
@@ -7,7 +7,7 @@ import { PrismaClient } from '@prisma/client';
 // =====================================
 const RESET_CONFIG = {
   hour: 12,        // 11h30 pour le reset (test)
-  minute: 20,      // 11h30 pour le reset (test)
+  minute: 29,      // 11h30 pour le reset (test)
   windowMinutes: 0 // Fenêtre de reset en minutes
 };
 
